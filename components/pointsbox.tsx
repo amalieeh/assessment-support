65cb26f5065f9563d0b2868c32eb434eaaca1c75
Sylvi Huynh <sylvi2602@hotmail.com>
2021-10-18 16:48:41 +0200

    add styling to pointbox and align it with title

--- components/pointsbox.tsx
+++ components/pointsbox.tsx
@@ -1,31 +1,52 @@
 import React, { useState } from 'react';
 import Select from 'react-select';
+import styles from '../styles/pointsbox.module.css'
 
 interface Option {
-  value: string;
+  value: number;
   label: string;
 }
 
-const options: Option[] = [
-  { value: '1p', label: '1p' },
-  { value: '2p', label: '2p' },
-  { value: '3p', label: '3p' },
-];
+//maxPoints: number
 
 const Pointsbox = () => {
+
+  // const start = 1
+
+  // function range(start, end) {
+  //   return Array(end - start + 1).fill().map((_, idx) => start + idx)
+  // }
+
+  const options: Option[] = [
+    { value: 1, label: '1 p' },
+    { value: 2, label: '2 p' },
+    { value: 3, label: '3 p' },
+  ];
+  
   const [selectedOption, setSelectedOption] = useState<Option | null>();
   console.log(selectedOption);
 
   return (
     <div className="App">
+      <div className={styles.container}>
       <Select
         defaultValue={selectedOption}
         onChange={setSelectedOption}
         options={options}
+        isClearable={true}
+        isSearchable={false} 
       />
+      </div>
     </div>
   );
 }
+
+
+
+
+
+ 
+
 
 export default Pointsbox;
 
--- pages/assessment.tsx
+++ pages/assessment.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next'
+import Pointsbox from '../components/pointsbox'
 import styles from '../styles/assessment.module.css'
 
 
@@ -12,34 +13,38 @@ const Assessment: NextPage = () => {
       <main className={styles.main}>
         <div className={styles.wrapper}>
         <div className={styles.grid}>
-          < div className={styles.card}>
-            <h2>Card 1 </h2>
+          <div className={styles.card}>
+            <div className={styles.alignTitlePoints}>
+              <h2>Card 1 </h2>
+              <Pointsbox />
+            </div>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
           </div>
         
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
+          <div className={styles.card}>
+            <div className={styles.alignTitlePoints}>
+              <h2>Learn &rarr;</h2>
+              <Pointsbox />
+            </div>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-          </a>
+          </div>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
+          <div className={styles.card}>
+          <div className={styles.alignTitlePoints}>
             <h2>Examples &rarr;</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-          </a>
+            <Pointsbox />    
+          </div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>    
+          </div>
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-            </p>
-          </a> 
+          <div className={styles.card}>
+            <div className={styles.alignTitlePoints}>
+                <h2>Deploy &rarr;</h2>
+                <Pointsbox />
+            </div>    
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+          </div> 
         </div>
         <div className={styles.next}>
         </div>
