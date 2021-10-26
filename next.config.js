867f6055dadabfd618d899ff761b46458f5c8e8d
Sylvi Huynh <sylvi2602@hotmail.com>
2021-10-26 11:52:13 +0200

    remove comments

--- next.config.js
+++ next.config.js
@@ -3,10 +3,3 @@ module.exports = {
   reactStrictMode: true,
 }
 
-// const withCSS = require('@zeit/next-css')
-
-// module.exports = withCSS({
-//   cssLoaderOptions: {
-//     url: false
-//   }
-// })
