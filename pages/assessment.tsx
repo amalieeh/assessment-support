f9a29980f4f9cc9b05091f7c023414fd515a084e
Sylvi Huynh <sylvihuynh@gmail.com>
2021-10-28 12:23:38 +0200

    add basic pagination logic: next and back

--- pages/assessment.tsx
+++ pages/assessment.tsx
@@ -1,11 +1,24 @@
 import type { NextPage } from 'next'
 import styles from '../styles/assessment.module.css'
 import Textbox from "../components/textbox";
+import { useState } from 'react';
 
 
 const Assessment: NextPage = () => {
 
-  let answers: string[] = ['hallo', 'hei', 'hade','a', 'b', 'c', 'd', 'e', 'f', 'hallo', 'hei', 'hade']
+  let answers: string[] = ['hallo', 'hei', 'hade','a', 'b', 'c', 'd', 'e', 'f', 'hallo', 'hei', 'hade', 'hallo', 'hei', 'hade','a', 'b', 'c', 'd', 'e', 'f', 'hallo', 'hei', 'hade', 'hallo', 'hei', 'hade','a', 'b', 'c', 'd', 'e', 'f', 'hallo', 'hei', 'hade']
+
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [maxItemsPerPage, setMaxItemsPerPage] = useState<number>(8);
+
+  const changePage = (direction: string) : void => {
+    if (direction == 'back') {
+      setCurrentPage(currentPage - 1) 
+    } else if (direction == 'next') {
+      setCurrentPage(currentPage + 1)
+    }
+  }
+
 
   return (
     <div className={styles.container}>
@@ -15,13 +28,20 @@ const Assessment: NextPage = () => {
       <main className={styles.main}>
         <div className={styles.wrapper}>
           <div className={styles.grid}>
-            {answers.map((answer: string) => <Textbox text={answer}/>)}          
+            {answers.slice((currentPage * maxItemsPerPage) - maxItemsPerPage, currentPage * maxItemsPerPage ).map((answer: string) => <Textbox text={answer}/>)}          
           </div>
           <div className={styles.next}/>
-        </div>
+        </div> 
       </main>
       <footer className={styles.footer}> 
-         <h1> . </h1>
+        <div>
+            {currentPage > 1? 
+              <button onClick={() => changePage('back')}>back</button>
+            : null}
+            {answers.length - 1 > currentPage * maxItemsPerPage ? 
+              <button onClick={() => changePage('next')}>next</button>
+            : null}
+        </div>
       </footer>
     </div>
   )
