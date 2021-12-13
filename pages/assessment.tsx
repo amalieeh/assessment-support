df462c57e9e7ed6d179c004365630562fff44a83
Sylvi Huynh <sylvihuynh@gmail.com>
2021-12-13 16:52:24 +0100

    style for report

--- pages/assessment.tsx
+++ pages/assessment.tsx
@@ -8,13 +8,14 @@ import cleanHtmlText from "../functions/helpFunctions";
 
 const Assessment: NextPage = () => {
 
-  const content: string =   a.ext_inspera_candidates[0].result.ext_inspera_questions[0].ext_inspera_candidateResponses[0].ext_inspera_response!
+  const answer_0: string = cleanHtmlText(a.ext_inspera_candidates[7].result.ext_inspera_questions[0].ext_inspera_candidateResponses[0].ext_inspera_response!)
+  const answer_1: string = cleanHtmlText(a.ext_inspera_candidates[1].result.ext_inspera_questions[0].ext_inspera_candidateResponses[0].ext_inspera_response!)
+  const answer_2: string = cleanHtmlText(a.ext_inspera_candidates[2].result.ext_inspera_questions[0].ext_inspera_candidateResponses[0].ext_inspera_response!)
+  const answer_3: string = cleanHtmlText(a.ext_inspera_candidates[3].result.ext_inspera_questions[0].ext_inspera_candidateResponses[0].ext_inspera_response!)
+  const answer_4: string = cleanHtmlText(a.ext_inspera_candidates[0].result.ext_inspera_questions[0].ext_inspera_candidateResponses[0].ext_inspera_response!)
 
-  const string1: string = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-  const string2: string = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
 
-  const answers: string[] = [content, cleanHtmlText(content), string1, string2, 'b', 'c', 'd', 'e', 'f', 'hallo', 'hei', 'hade', 'hallo', 'hei', 'hade','a', 'b', 'c', 'd', 'e', 'f', 'hallo', 'hei', 'hade', 'hallo', 'hei', 'hade','a', 'b', 'c', 'd', 'e', 'f', 'hallo', 'hei', 'hade']
-
+  const answers: string[] = [answer_0, answer_1, answer_2, answer_3, answer_4, 'a','b','c']
   const [currentPage, setCurrentPage] = useState<number>(1);
   const [maxItemsPerPage, setMaxItemsPerPage] = useState<number>(4);
 
@@ -29,7 +30,7 @@ const Assessment: NextPage = () => {
   return (
     <div className={styles.container}>
         <h1>
-            Eksamen TDT100
+            {a.ext_inspera_assessmentRunTitle}
         </h1>
       <main className={styles.main}>
         {/* <div className={styles.wrapper}> */}
