1602d953670d631fdcd49826201999c67de178a8
Amalie Henni <amalie.henn@gmail.com>
2022-02-02 10:51:17 +0100

    make title seem more clickable

--- components/header.tsx
+++ components/header.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import {inspect} from "util";
 import styles from '../styles/Header.module.css'
-import data from "../data/IT2810Høst2018.json";
 import Link from "next/link";
 
 interface Headerprops {
-  data: any
-  taskNumber?: number
+  data: any;
+  taskNumber?: number;
 }
 
 const Header: React.FC<Headerprops> = (props: Headerprops) => {
@@ -14,11 +12,11 @@ const Header: React.FC<Headerprops> = (props: Headerprops) => {
   return (
     <div className={styles.headerstyle}>
       <Link href="/">
-        <h1>
+        <h1 className={styles.makeClickable}>
           {props.data.ext_inspera_assessmentRunTitle}
         </h1>
       </Link>
-        {props.taskNumber && 1 <= props.taskNumber ?
+        {props.taskNumber && props.taskNumber >= 1 ?
           <h2>
             Oppgave {props.data.ext_inspera_candidates[0].result.ext_inspera_questions[props.taskNumber-1].ext_inspera_questionNumber}
           </h2>
