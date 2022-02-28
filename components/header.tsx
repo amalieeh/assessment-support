bab8e491ed94edebacf5db56a4f903b0480b5bb6
Sylvi Huynh <sylvihuynh@gmail.com>
2022-02-28 15:45:51 +0100

    make header more generic

--- components/header.tsx
+++ components/header.tsx
@@ -4,18 +4,20 @@ import Link from "next/link";
 
 interface Headerprops {
   data: any;
-  taskNumber: number;
+  taskNumber?: number;
   description?: string;
 }
 
 const Header: React.FC<Headerprops> = (props: Headerprops) => {
   return (
     <div className={styles.headerstyle}>
-      {props.description ? (
+      {props.taskNumber && props.description ? (
         <h1>
-          Oppgave {props.taskNumber}: {props.description}
+          Oppgave {props.taskNumber} : {props.description}
         </h1>
-      ) : null}
+      ) : (
+        <h1>{props.description}</h1>
+      )}
       <Link href="/">
         <h2 className={styles.makeClickable}>
           {props.data.ext_inspera_assessmentRunTitle}
