537464e18158c17920f285d7dbc9d98b71d77bde
Sylvi Huynh <sylvihuynh@gmail.com>
2022-03-03 13:54:06 +0100

    add scrolling to textbox and resize pointsbox

--- components/textbox.tsx
+++ components/textbox.tsx
@@ -1,26 +1,25 @@
-import * as React from 'react';
+import * as React from "react";
 import styles from "../styles/Textbox.module.css";
 import Pointsbox from "./pointsbox";
-import {AssessmentType} from "../types/Types";
-
+import { AssessmentType } from "../types/Types";
 
 interface textboxprop {
-    assessment: AssessmentType;
-    setAssessment: (assessment: AssessmentType, newScore: number) => void;
+  assessment: AssessmentType;
+  setAssessment: (assessment: AssessmentType, newScore: number) => void;
 }
 
 const Textbox: React.FC<textboxprop> = (props: textboxprop) => {
-    return (
-        <div className={styles.card}>
-            <div className={styles.alignTitlePoints}>
-                <Pointsbox
-                    assessment={props.assessment}
-                    setAssessment={props.setAssessment}
-                />
-            </div>
-            {props.assessment.answer}
-        </div>
-    )
+  return (
+    <div className={styles.card}>
+      <div className={styles.alignTitlePoints}>
+        <Pointsbox
+          assessment={props.assessment}
+          setAssessment={props.setAssessment}
+        />
+      </div>
+      {props.assessment.answer}
+    </div>
+  );
 };
 
 export default Textbox;
